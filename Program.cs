665ee0b8d771792fbef47c4b30a1bbde9054919a
Rodrigo Alves <rodrigorac.2003@gmail.com>
2021-03-08 15:16:45 -0300

    Programa O GRITADOR

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace O_Gritador
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+           string QualquerCoisaSerDigitada;
+
+           Console.WriteLine();
+           Console.WriteLine();
+           Console.ForegroundColor=ConsoleColor.Cyan;
+           Console.WriteLine("|---GRITADOR---");
+           Console.WriteLine("|Digite algo a ser repetido, gritando (em CAIXA ALTA).");
+           Console.ForegroundColor=ConsoleColor.White;
+           Console.WriteLine();
+           Console.WriteLine();
+
+
+            
+            Console.WriteLine("- Por favor digite algo para prosseguir...");
+            Console.WriteLine();
+
+            QualquerCoisaSerDigitada = Console.ReadLine();
+            Console.WriteLine();
+
+            
+            Console.WriteLine($"{QualquerCoisaSerDigitada.ToUpper()}");
+            
+            
+            
+            
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
